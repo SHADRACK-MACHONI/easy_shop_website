97b5f55302e1bb3a559a8668be2f831c25608844
SHADRACK MACHONI <shadrackmachoni26@gmail.com>
2025-07-05 03:41:31 +0300

    Fix models.py import and definition

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .product import Product
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
